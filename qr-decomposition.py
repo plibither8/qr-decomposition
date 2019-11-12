d1f40c6f064ca88bbbe43833ad481d02622c58b0
Mihir Chaturvedi <plibither8@gmail.com>
2019-11-12 21:40:39 +0530

    Fix: Implement with python

---
+++ qr-decomposition.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+m = int(input("Enter m: "))
+n = int(input("Enter n: "))
+
+print("Start entering rows of the matrix:")
+
+A = []
+
+for i in range(m):
+    A.append(np.array(list(map(float, input().strip().split()[:n]))))
+
+A = np.array(A)
+
+def getQ(M):
+    Q = []
+    M = np.transpose(M)
+
+    for i in range(len(M)):
+        v = np.copy(M[i])
+        for j in range(i):
+            v -= (np.dot(M[i], Q[j]) / np.dot(Q[j], Q[j])) * Q[j]
+        Q.append(v)
+
+    Q = np.array(list(map(lambda v: v / sum(v**2)**0.5, Q)))
+    Q = Q.transpose()
+
+    return Q
+
+def getR(M, Q):
+    Q_t = np.transpose(Q)
+    return np.matmul(Q_t, M)
+
+Q = getQ(A)
+R = getR(A, Q)
+
+print("\nQ:\n", Q)
+print("\nR:\n", R)
